bc425595599577e2dcf1ceeaf7d4f38cfdfb4465
lykesun <553905001@qq.com>
2016-02-28 00:07:46 +0800

    完成遍历省市县三级列表的功能。

--- src/db/CoolWeatherOpenHelper.java
+++ src/db/CoolWeatherOpenHelper.java
@@ -29,7 +29,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
 			"id integer primary key autoincrement," +
 			"county_name text," +
 			"county_code text," +
-			"province_id integer)";
+			"city_id integer)";
 	
 	public CoolWeatherOpenHelper(Context context, String name,
 			CursorFactory factory, int version) {
